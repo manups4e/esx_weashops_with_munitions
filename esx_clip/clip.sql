845a6a48f71e0e1b22ed6c9824e7c93b6f16e394
leonardo emanuele <manups4e@gmail.com>
2018-06-20 18:13:43 +0200

    Added Files

---
+++ esx_clip/clip.sql
@@ -0,0 +1,7 @@
+ INSERT INTO `items` (name, label, `limit`) VALUES
+  ('Caricatore di Munizioni','clip', 2)
+ ;
+ 
+ INSERT INTO `shops` (name, item, `price`) VALUES
+  ('TwentyFourSeven', 'clip', 3000)
+ ;
---
+++ esx_weashops/locales/en.lua
@@ -0,0 +1,13 @@
+Locales ['en'] = {
+
+  ['buy_license'] = 'Comprare una licenza?',
+  ['yes'] = 'Sì',
+  ['no'] = 'No',
+  ['buy'] = 'Hai comprato',
+  ['not_enough_black'] = 'Non hai abbastanza denaro sporco',
+  ['not_enough'] = 'Non hai abbastanza denaro',
+  ['shop'] = 'Negozio',
+  ['shop_menu'] = 'Premi ~INPUT_CONTEXT~ per accedere al negozio.',
+  ['map_blip'] = 'Negozio armi',
+
+}
